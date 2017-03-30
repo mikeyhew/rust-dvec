5337789897c280203e6eb3c0871825461c71f4bf
Michael Hewson <michael@michaelhewson.ca>
2017-03-30 19:16:10 -0400

    I did a bunch of work and got things to type check, and now borrowck is complaining

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,82 @@
 use std::marker::PhantomData;
 use std::mem;
+use std::ptr;
 
-pub struct DVec<T> {
-    data: *mut T,
-    length: usize,
-    offset: usize,
+/// An uninitialized buffer of `capacity * mem::size_of<T>()` bytes.
+struct Buffer<T> {
+    ptr: *mut T,
     capacity: usize,
 }
 
-impl<T> DVec<T> {
-    pub fn with_capacity(capacity: usize) -> DVec<T> {
-        let p: *mut T = unsafe {
-            let mut v = Vec::<T>::with_capacity(capacity);
-            v.set_len(capacity);
-            let mut b = v.into_boxed_slice();
-            let p = b.as_mut().as_mut_ptr();
-            mem::forget(b);
-            p
-        };
+impl<T> Buffer<T> {
+    fn new(capacity: usize) -> Buffer<T> {
+        let mut v = Vec::<T>::with_capacity(capacity);
+        let ptr = v.as_mut_ptr();
+        mem::forget(v);
+        Buffer {
+            ptr: ptr,
+            capacity: capacity,
+        }
+    }
+}
 
+impl<T> Drop for Buffer<T> {
+    fn drop(&mut self) {
+        unsafe {
+            mem::drop(Vec::from_raw_parts(self.ptr, 0, self.capacity));
+        };
+    }
+}
+
+struct DVec<T> {
+    buf: Buffer<T>,
+    offset: usize,
+    length: usize,
+}
+
+impl<T> DVec<T> {
+    pub fn new() -> DVec<T> {
+        DVec::with_capacity(0)
+    }
+
+    pub fn with_capacity(capacity: usize) -> DVec<T> {
         DVec {
-            data: p,
+            buf: Buffer::<T>::new(capacity),
             length: 0,
             offset: capacity/2,
-            capacity: capacity,
+        }
+    }
+
+    fn resize(&mut self, new_capacity: usize) {
+        assert!(new_capacity >= self.length);
+
+        // This *could* panic, and I don't know much about panic safety,
+        // but it would probably be better if it panics before we change
+        // things inside `self`.
+        let new_buf = Buffer::new(new_capacity);
+
+        let old_buf = self.buf;
+        let old_offset = self.offset;
+
+        self.buf = new_buf;
+        self.offset = (new_capacity - self.length) / 2;
+
+        unsafe {
+            ptr::copy(
+                old_buf.ptr.offset(old_offset as isize),
+                self.buf.ptr.offset(self.offset as isize),
+                self.length
+            );
         }
     }
 }
 
 impl<T> Drop for DVec<T> {
     fn drop(&mut self) {
-        let mut v = unsafe {
-            Vec::from_raw_parts(self.data, self.capacity, self.capacity)
-        };
-        let mut i: usize = 0;
-        for x in v {
-            if i < self.offset {
-                mem::forget(x);
-            } else if i < self.offset + self.length {
-                mem::drop(x);
-            } else if i < self.capacity {
-                mem::forget(x);
-            } else {
-                unreachable!();
+        for i in 0..self.length {
+            unsafe {
+                mem::drop(*self.buf.ptr.offset((self.offset + i) as isize));
             }
-            i += 1;
         }
     }
 }
@@ -54,7 +86,7 @@ mod tests {
     use super::DVec;
     #[test]
     fn it_works() {
-        DVec::<usize>::with_capacity(0);
+        DVec::<usize>::new();
         DVec::<usize>::with_capacity(20);
     }
 }
